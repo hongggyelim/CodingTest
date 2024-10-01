58d27d11065627a6183acfabfe47dbc79475343f
yelim <167871589+hongggyelim@users.noreply.github.com>
2024-10-02 01:36:26 +0900

    [level 0] Title: 합성수 찾기, Time: 0.23 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.js
@@ -0,0 +1,16 @@
+function solution(n) {
+    let answer = 0;
+    for(let i = 1; i <= n ; i++){
+        let count = 0;
+        for (let j = 1; j <= i; j ++){
+            if(i % j === 0){
+                count += 1;
+                if(count >= 3){
+                    answer += 1;
+                    break;
+                } 
+            }   
+        }
+    }
+    return answer;
+}
