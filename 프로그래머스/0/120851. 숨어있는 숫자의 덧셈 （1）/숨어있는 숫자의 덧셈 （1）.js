3a61b3d14b7b06939f8bc57c9542fd656e87ac48
yelim <167871589+hongggyelim@users.noreply.github.com>
2024-10-04 14:52:57 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
@@ -1,4 +1,3 @@
 function solution(my_string) {
-    const num = '1234567890';
-    return a = [...my_string].filter((c)=> num.includes(c)).reduce((a, b)=> a*1 + b*1, 0)
+    return a = [...my_string].filter((c)=> !isNaN(c)).reduce((a, b)=> a*1 + b*1, 0)
 }
