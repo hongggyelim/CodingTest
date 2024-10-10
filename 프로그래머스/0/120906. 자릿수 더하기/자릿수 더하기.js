df46f974cc71ecc8dca0116d88f056e421f022e0
yelim <167871589+hongggyelim@users.noreply.github.com>
2024-10-10 14:23:01 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.js
@@ -1,5 +1,5 @@
 function solution(n) {
     var answer = 0;
-    return [...String(n)].reduce((acc, el)=> acc*1 +el*1, 0)
+    return [...String(n)].reduce((acc, el)=> acc + +el, 0)
     return answer;
 }
