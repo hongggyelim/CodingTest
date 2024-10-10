d7cecdca72b15f8fc222d0ca0061e9527e173673
yelim <167871589+hongggyelim@users.noreply.github.com>
2024-10-10 15:27:18 +0900

    [level 0] Title: 직사각형 넓이 구하기, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.js
@@ -0,0 +1,8 @@
+function solution(dots) {
+    var answer = 0;
+    const [[x1, y1], [x2, y2], [x3, y3], [x4, y4]] = dots
+    let length = Math.abs(x1 - x2) || Math.abs(x2 - x3) ;
+    let height = Math.abs(y3 - y2) || Math.abs(y3 - y4) ;
+    answer = length * height;
+    return answer;
+}
