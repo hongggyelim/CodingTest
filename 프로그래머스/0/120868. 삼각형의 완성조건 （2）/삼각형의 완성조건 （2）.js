e2fff00ccbb48be9824f57dfc64d2940819e27b9
yelim <167871589+hongggyelim@users.noreply.github.com>
2024-10-12 18:02:37 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 33.07 ms, Memory: 34.9 MB -BaekjoonHub

--- 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.js
+++ 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.js
@@ -2,19 +2,14 @@ function solution(sides) {
     var answer = 0;
     // 반복하면서 sort()로 계속 내림차순 정렬해주고, 가장 큰값 [0] 이 나머지 [1]+ [2] 보다 작은지 확인 
     let initialX = sides[0] + sides[1];
-            console.log(`최초 initialX : ${initialX}, sides: ${JSON.stringify(sides)}`)
-    let newArr;
     while(initialX > 0){
-        let x = initialX;
-        let arr = sides;
-        arr.push(x);
-        arr.sort((a, b)=> b - a);
-        console.log(`x: ${x}, arr: ${JSON.stringify(arr)}`)
+        let arr = [...sides, initialX].sort((a, b)=> b - a);
         // x 가 가능한 정수인지 검증
         if (arr[0] < arr[1] + arr[2]){
             answer ++;
         } 
-        let index = arr.indexOf(x);
+        // splice 대신 filter 로 x 를 제거하면 최초 sides 배열에 있던 값 중 x와 동일한 값이 함께 삭제됨 
+        let index = arr.indexOf(initialX);
         arr.splice(index, 1);
         initialX --;
         console.log(`if문 끝나고 initialX : ${initialX} arr: ${JSON.stringify(arr)} answer =${answer}`)
