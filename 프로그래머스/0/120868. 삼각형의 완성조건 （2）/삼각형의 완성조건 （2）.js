e18f83a384cb00d6ae7df55b0ebed29d77468768
yelim <167871589+hongggyelim@users.noreply.github.com>
2024-10-15 14:09:35 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 66.88 ms, Memory: 39.5 MB -BaekjoonHub

--- 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.js
+++ 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.js
@@ -4,15 +4,12 @@ function solution(sides) {
     let initialX = sides[0] + sides[1];
     while(initialX > 0){
         let arr = [...sides, initialX].sort((a, b)=> b - a);
+        console.log(arr);
         // x 가 가능한 정수인지 검증
         if (arr[0] < arr[1] + arr[2]){
             answer ++;
         } 
-        // splice 대신 filter 로 x 를 제거하면 최초 sides 배열에 있던 값 중 x와 동일한 값이 함께 삭제됨 
-        let index = arr.indexOf(initialX);
-        arr.splice(index, 1);
         initialX --;
-        console.log(`if문 끝나고 initialX : ${initialX} arr: ${JSON.stringify(arr)} answer =${answer}`)
     }
     return answer;
 }
