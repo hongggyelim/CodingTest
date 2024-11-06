bf05e7de9b304e64db24fb32ba229883804bf460
yelim <167871589+hongggyelim@users.noreply.github.com>
2024-11-06 13:51:34 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.js
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.js
@@ -1,3 +1,3 @@
 function solution(my_string) {
-    return [...my_string].map((v)=> v === v.toUpperCase() ? v.toLowerCase() : v).sort().join('');
+    return [...my_string.toLowerCase()].sort().join('');
 }
