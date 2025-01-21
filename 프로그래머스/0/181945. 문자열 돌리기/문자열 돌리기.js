17aeb71e6419d7fdd05e9316a2c90aa26a6cac40
yelim <167871589+hongggyelim@users.noreply.github.com>
2025-01-22 08:23:21 +0900

    [level 0] Title: 문자열 돌리기, Time: 300.19 ms, Memory: 31.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.js
@@ -0,0 +1,14 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = [line];
+}).on('close',function(){
+    arr = input[0].split("")
+    arr.map((char)=>console.log(char))
+});
