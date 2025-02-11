bc6d08f8c613189498927ff3555e77f96171bc25
yelim <167871589+hongggyelim@users.noreply.github.com>
2025-02-12 08:08:38 +0900

    [level 0] Title: 배열 만들기 5, Time: 1.53 ms, Memory: 41 MB -BaekjoonHub

--- 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.js
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.js
@@ -1,5 +1,3 @@
 function solution(intStrs, k, s, l) {
-    var answer = [];
-    const newArr = intStrs.map((v, i)=> v.slice(s, s+l))
-    return newArr.filter((v)=> v > k).map((v)=>Number(v))
+    return  intStrs.map((v, i)=> +v.slice(s, s+l)).filter((v)=> v > k)
 }
