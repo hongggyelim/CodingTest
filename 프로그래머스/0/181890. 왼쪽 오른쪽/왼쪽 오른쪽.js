f093ddc832b4e5ca49fa18d8d4e432276bc20cf4
yelim <167871589+hongggyelim@users.noreply.github.com>
2025-02-21 08:32:07 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
@@ -2,8 +2,6 @@ function solution(str_list) {
     const right = str_list.indexOf("r"); 
     const left = str_list.indexOf("l");
     if (right === -1 && left === -1) return [];
-    if (left === 0) return [];
-    
     if(right === -1 || left === -1){
         return (right < left) ? str_list.slice(0, left) : str_list.slice(right + 1)
     }else{
