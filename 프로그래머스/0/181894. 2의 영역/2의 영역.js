a10f15a5777ac15e14a27f6f17f24a1fd322ddaf
yelim <167871589+hongggyelim@users.noreply.github.com>
2025-02-21 07:41:11 +0900

    [level 0] Title: 2의 영역, Time: 5.96 ms, Memory: 43.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.js
@@ -0,0 +1,11 @@
+function solution(arr) {
+    // 2를 filter -> index => min, max 구해서 원본 arr에 index 로 slice 
+    // 2부터 2까지 
+    const num2range = [];
+    arr.filter((v, i)=>v === 2 ? num2range.push(i) : v);
+    const min = Math.min(...num2range);
+    const max = Math.max(...num2range);
+    if (!num2range.length) return [-1]
+    return arr.slice(min, max+1)
+               
+}
