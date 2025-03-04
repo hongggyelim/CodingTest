09ad20693c7d13b8c297b1d795a698d89b2010cf
yelim <167871589+hongggyelim@users.noreply.github.com>
2025-03-05 07:55:22 +0900

    [level 0] Title: 무작위로 K개의 수 뽑기, Time: 3.05 ms, Memory: 35.8 MB -BaekjoonHub

--- 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.js
+++ 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.js
@@ -2,8 +2,5 @@ function solution(arr, k) {
     // Set 으로 중복 제거
     let set = Array(...new Set(arr)).slice(0, k);
     
-    while(set.length < k){
-        set.push(-1)
-    }
-    return set;
+    return [...set, ...Array(k - set.length).fill(-1)]
 }
