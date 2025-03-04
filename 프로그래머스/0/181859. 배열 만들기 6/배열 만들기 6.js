81f59cf4a4b138009ce38283b48b529ca0408434
yelim <167871589+hongggyelim@users.noreply.github.com>
2025-03-05 07:43:42 +0900

    [level 0] Title: 배열 만들기 6, Time: 63.26 ms, Memory: 63.6 MB -BaekjoonHub

--- 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.js
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.js
@@ -1,10 +1,11 @@
 function solution(arr) {
     var answer = [];
     // 연속한 0이나 1이 2개 만나면 추가, 삭제-> 없음
-    for (let i = 0; i < arr.length; i ++){
-        answer.length === 0 ? answer.push(arr[i]) : 
-        answer.at(-1) === arr[i] ? answer.pop() :
-        answer.push(arr[i])
-    }
+    
+    arr.forEach((v)=> {
+        answer.length === 0 ? answer.push(v) : 
+        answer.at(-1) === v ? answer.pop() :
+        answer.push(v)
+    })
     return answer.length > 0 ? answer : [-1];
 }
