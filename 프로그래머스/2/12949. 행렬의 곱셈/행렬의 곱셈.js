c60a5858c707f7f9ee034e4ceda303ca077be149
yelim <167871589+hongggyelim@users.noreply.github.com>
2025-04-07 15:18:05 +0900

    [level 2] Title: 행렬의 곱셈, Time: 6.55 ms, Memory: 38.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
@@ -0,0 +1,18 @@
+function solution(arr1, arr2) {
+    var answer = [];
+    for (let i = 0; i < arr1.length; i++){
+        let sum = [];
+        
+        for(let j = 0; j < arr2[0].length; j++){
+            let el = 0;
+            
+            for (let k = 0; k < arr2.length; k++){
+                el += arr1[i][k] * arr2[k][j];
+                
+            }   
+            sum.push(el)
+        }
+        answer.push(sum)
+    } 
+    return answer;
+}
