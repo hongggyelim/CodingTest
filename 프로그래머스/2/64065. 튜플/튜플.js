87e779f291404b250a257377ea98c90cbe5c6eea
yelim <167871589+hongggyelim@users.noreply.github.com>
2025-04-29 10:54:17 +0900

    [level 2] Title: 튜플, Time: 71.26 ms, Memory: 52.5 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.js
+++ 프로그래머스/2/64065. 튜플/튜플.js
@@ -1,22 +1,18 @@
 function solution(s) {
-    var answer = [];
     s = s.replace("{{","").replace("}}","").split("},{").map((v)=> v.split(",")).map((v)=> v.map((w)=> Number(w))).sort((a, b)=> a.length - b.length);
-
+    console.log(s)
     let prev = s[0].pop()
     let history = [];
     history.push(prev);
     
-    for(let arr of s){
-            // arr 원소가 2개 이상일때
-            // 새로 추가된 값 el 
-        if(arr.length >= 2){
-            for (el of arr){
-                if(!history.includes(el)){
-                    prev= el
-                    history.push(prev)
-                    }
+    for(let i = 1; i < s.length; i++){
+    // arr 원소가 2개 이상일때
+    // 새로 추가된 값 el 
+        for (el of s[i]){
+            if(!history.includes(el)){
+                history.push(el)
+                }
             }
-        }
     }
     return history;
 }
