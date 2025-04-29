77ffb0cfa2ef25b11dd4624bec6f7c699d1ecd5e
yelim <167871589+hongggyelim@users.noreply.github.com>
2025-04-29 22:50:24 +0900

    [level 2] Title: 프로세스, Time: 0.41 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.js
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -1,26 +1,23 @@
 function solution(arr, location) {
     var answer = 0; // 실행된 프로세스 개수
     let max;
-    if (location === arr.indexOf(max)) return 1;
-    let target = location;
     
     while(arr.length > 0){
         max = Math.max(...arr)
         let first = arr.shift();
         if(first === max){
             answer ++;
-            console.log("최대값 -> 실행", answer)
-            if(target === 0){
+            if(location === 0){
                 return answer;
             }else{
-                target --;
+                location --;
             }
         }else{
             arr.push(first);
-            if(target === 0){
-                target = arr.length -1
+            if(location === 0){
+                location = arr.length -1
             }else{
-                target --;
+                location --;
             }
         }
 }return answer;
