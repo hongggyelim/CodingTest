e91d6c01a1c79e91009c4e3616b52c86206e181e
yelim <167871589+hongggyelim@users.noreply.github.com>
2025-05-02 20:05:49 +0900

    [level 1] Title: 문자열 나누기, Time: 3.87 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.js
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.js
@@ -2,28 +2,20 @@ function solution(s) {
     let x = s[0]
     let sameCount = 0;
     let diffCount = 0;
-    let index= 0;
-    let word = [...s].join("")
     let answer = 0;
     
     for(let i = 0; i < s.length; i++){
         s[i] === x ? sameCount ++ : diffCount ++
-        // console.log(s[i], sameCount, diffCount)
+        
         if(sameCount === diffCount){
-            index = i
-            word = s.slice(index + 1)
-            
-            x = word[0]
-            // console.log("분해!","word", word, x)
+            x = s[i + 1]
             answer ++
             sameCount = 0;
             diffCount = 0;
         }
-        if(i === s.length -1 && sameCount){
-            // console.log("마지막 인덱스")
+    }
+     if(diffCount || sameCount){
             answer ++
         }
-        
-    }
     return answer;
 }
