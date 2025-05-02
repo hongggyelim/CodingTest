88c7423fa093ca24905fb6e15750ab15df67aa51
yelim <167871589+hongggyelim@users.noreply.github.com>
2025-05-02 19:30:20 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 2.25 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -1,26 +1,19 @@
 function solution(lottos, win_nums) {
-    var answer = [];
-    let notCorrect = lottos.filter((v)=> !win_nums.includes(v))
-//     if(notCorrect.length === 0) return [1,1]
     
-//     if(notCorrect.reduce((a,b)=> a+b)=== 0) return [1,6]
+    // 인덱스 = 맞춘 개수, 값 = 순위
+    const score = [6, 6, 5, 4, 3, 2, 1]
     
-    let notCorrectCount = notCorrect.length; // 4개 
-    let zeroCount = notCorrect.filter((v)=> v === 0).length; // 2개
-    let minCorrect = 6 - notCorrectCount
+    let notCorrect = 0
+    let zeroCount = 0
+    let correct = 0
     
-    const winDict = {
-        // 일치 개수 : 순위
-        6: 1, 
-        5: 2,
-        4: 3, 
-        3: 4, 
-        2: 5, 
-        1: 6,
-        0: 6
-    }
-    let min = winDict[minCorrect];
-    let max = winDict[minCorrect + zeroCount];
     
-    return [max, min];
+    lottos.forEach((v)=> {
+        v === 0 ? zeroCount ++ : win_nums.includes(v)? correct ++: v
+    })
+    notCorrect -= zeroCount
+
+    
+    console.log(zeroCount + correct, correct)
+    return [score[zeroCount + correct], score[correct]];
 }
