e03d406664a11959258b40a127708dd76633408a
yelim <167871589+hongggyelim@users.noreply.github.com>
2025-05-02 17:52:09 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.js
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.js
@@ -3,19 +3,23 @@ function solution(arr) {
     let temp=''
     let score = 0;
     let scoreList = [];
-    
+    const bonus = ["S", "D", "T"]
     for(let i = 0; i < arr.length; i++){
         
         if(!isNaN(Number(arr[i]))){
             temp += arr[i]
         }
-        console.log(arr[i],temp)
+        
+        if(bonus.includes(arr[i])){
+            switch(arr[i]){
+                case "S": score = Number(temp) ** 1; break;
+                case "D": score = Number(temp) ** 2; break;
+                case "T": score = Number(temp) ** 3; break;
+                }
+            temp = ''; scoreList.push(score); 
+        }
+        
         switch(arr[i]){
-                
-            case "S": score = Number(temp) ** 1; temp = ''; scoreList.push(score); break;
-            case "D": score = Number(temp) ** 2; temp = ''; scoreList.push(score); break;
-            case "T": score = Number(temp) ** 3; temp = ''; scoreList.push(score); break;
-                
             case "*": {
                 let last = scoreList.splice(-2)
                 scoreList.push(...last.map((v)=> v*2))
@@ -27,8 +31,7 @@ function solution(arr) {
                 break;}
         }
     
-    
     }
-    console.log(scoreList)
+    // console.log(scoreList)
     return scoreList.reduce((a,c)=> a+c)
 }
